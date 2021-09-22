ac142eae4e7f613f9f9c5ca7610ae915f7ef8338
roydesignabad <30890186+roydesignabad@users.noreply.github.com>
2021-09-22 10:13:19 +0530

    divide year

---
+++ yearto12month.py
@@ -0,0 +1,33 @@
+import datetime
+
+# borrowed from https://stackoverflow.com/a/13565185
+# as noted there, the calendar module has a function of its own
+def last_day_of_month(any_day):
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
+    return next_month - datetime.timedelta(days=next_month.day)
+
+begin = "2020-01-01"
+end = "2020-10-30"
+
+def monthlist(begin,end):
+    begin = datetime.datetime.strptime(begin, "%Y-%m-%d")
+    end = datetime.datetime.strptime(end, "%Y-%m-%d")
+
+    result = []
+    while True:
+        if begin.month == 12:
+            next_month = begin.replace(year=begin.year+1,month=1, day=1)
+            print("12 month",next_month)
+        else:
+            next_month = begin.replace(month=begin.month+1, day=1)
+            print("11 month",next_month)
+        if next_month > end:
+            break
+        result.append ([begin.strftime("%Y-%m-%d"),last_day_of_month(begin).strftime("%Y-%m-%d")])
+        begin = next_month
+    result.append ([begin.strftime("%Y-%m-%d"),end.strftime("%Y-%m-%d")])
+    return result
+
+
+date_list = monthlist(begin,end)
+print (date_list)
